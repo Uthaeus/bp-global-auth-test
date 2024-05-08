a4d8c35840256f225b3d55c54e28465730e26681
Uthaeus <romanlavery@gmail.com>
2024-05-08 16:49:25 -0600

    created root layout and home component, created initial router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import RootLayout from "./components/layouts/root-layout";
+import Home from "./pages/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  }
+])
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
