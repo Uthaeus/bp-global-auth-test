39b222ee957c2a5ea8905d36c150f08ea3cbb3f2
Uthaeus <romanlavery@gmail.com>
2024-05-11 20:32:05 -0600

    created users context provider, imported into index

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.scss';
 import App from './App';
 
 import UserContextProvider from './store/user-context';
+import UsersContextProvider from './store/users-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserContextProvider>
-      <App />
+      <UsersContextProvider>
+        <App />
+      </UsersContextProvider>
     </UserContextProvider>
   </React.StrictMode>
 );
---
+++ src/store/users-context.js
@@ -0,0 +1,53 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+import { UserContext } from "./user-context";
+import { dummyUsers } from "./dummy/dummy-users";
+
+export const UsersContext = createContext({
+
+    users: [],
+    addUser: () => {},
+    updateUser: () => {},
+    deleteUser: () => {},
+});
+
+export const UsersContextProvider = ({ children }) => {
+
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const { isAdmin } = useContext(UserContext);
+
+    useEffect(() => {
+        
+        if (isAdmin) {
+            setUsers(dummyUsers);
+        }
+
+        setIsLoading(false);
+    }, []);
+
+    const addUser = (data) => {
+
+        setUsers([...users, data]);
+    }
+
+    const updateUser = (id, data) => {
+
+        setUsers(users.map(u => u.id === id ? { ...u, ...data } : u));
+    }
+
+    const deleteUser = (id) => {
+
+        setUsers(users.filter(u => +u.id !== +id));
+    }
+
+    const value = {
+
+        users,
+        addUser,
+        updateUser,
+        deleteUser
+    }
+
+    return <UsersContext.Provider value={value}>{!isLoading && children}</UsersContext.Provider>
+}
